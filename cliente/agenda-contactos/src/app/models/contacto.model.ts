3d5aa6e5fd1599077fa1d3e780dd80970cadf546
José Miguel <josmilope@gmail.com>
2016-12-15 16:12:54 +0100

    falta coma en modelo

--- cliente/agenda-contactos/src/app/models/contacto.model.ts
+++ cliente/agenda-contactos/src/app/models/contacto.model.ts
@@ -5,7 +5,7 @@ export class Contacto{
 		public primer_apellido:string,
 		public segundo_apellido:string,
 		public fecha_nacimiento:string,
-		public telefono_fijo:string
+		public telefono_fijo:string,
 		public telefono_movil:string
 	){}
 }
